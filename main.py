c8ee4a794d3ba4e04717396217a74689f4554b7c
Ferret182 <timo.leinders@icloud.com>
2026-06-14 13:41:50 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# main.py
+import os
 from fastapi import FastAPI
 from supabase import create_client
 
 app = FastAPI()
 
-SUPABASE_URL = "https://lxkvotweebvcsmzzlvgk.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4a3ZvdHdlZWJ2Y3NtenpsdmdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0Mjg1MjUsImV4cCI6MjA5NzAwNDUyNX0.-vyNrS08AiMSYcxWi8_-isKUuj-rTTSvdXfwptUeZEs"
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 db = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.get("/events")
